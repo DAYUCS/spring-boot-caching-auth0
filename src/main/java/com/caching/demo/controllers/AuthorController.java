72e974e95361d061998e5fba44fef5f6dcba6b7a
Dayu Bian <dayub@chinasystems.com>
2022-11-18 14:31:45 +0800

    Update Data and Cache

--- src/main/java/com/caching/demo/controllers/AuthorController.java
+++ src/main/java/com/caching/demo/controllers/AuthorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +21,23 @@ public class AuthorController {
 
     @GetMapping("/authors/{id}")
     public ResponseEntity<Author> findAuthorById(@PathVariable Integer id) {
-        loggingMessage("Request received!");
+        loggingMessage("Read Request received!");
         // retrieving the desired data
         Author author = authorService.retrieveById(id);
         loggingMessage("Data retrieved!");
+
+        return new ResponseEntity<>(
+                author,
+                HttpStatus.OK
+        );
+    }
+
+    @PutMapping("/authors/{id}")
+    public ResponseEntity<Author> replaceAuthorById(@PathVariable Integer id) {
+        loggingMessage("Modify Request received!");
+        // retrieving the desired data
+        Author author = authorService.replaceById(id);
+        loggingMessage("Data modified!");
 
         return new ResponseEntity<>(
                 author,
--- src/main/java/com/caching/demo/services/AuthorService.java
+++ src/main/java/com/caching/demo/services/AuthorService.java
@@ -2,23 +2,34 @@ package com.caching.demo.services;
 
 import com.caching.demo.data.Author;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorService {
 
+    private ConcurrentHashMap<Integer, Author> store = new ConcurrentHashMap<>();
+
     @Cacheable("authors")
     public Author retrieveById(Integer id) {
         // simulating a delay due to the data retrieval operation
         try {
             System.out.println("Retrieving the author...");
+            store.put(id, new Author(id, "Mary", "Williams", "1988-11-19"));
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // returning a list containing all the authors
-        return new Author(id, "Mary", "Williams", "1988-11-19");
+        return store.get(id);
+    }
+
+    @CachePut("authors")
+    public Author replaceById(Integer id) {
+        store.replace(id, new Author(id, "Tom", "Williams", "1978-11-19"));
+        return store.get(id);
     }
 }
